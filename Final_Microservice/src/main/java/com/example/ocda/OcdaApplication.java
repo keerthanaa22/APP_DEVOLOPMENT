d620fc692e6a717e9901da89add346822f58cd27
keerthanaa22 <67215306+keerthanaa22@users.noreply.github.com>
2023-07-28 11:54:35 +0530

    Add files via upload

---
+++ Final_Microservice/src/main/java/com/example/ocda/OcdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ocda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcdaApplication.class, args);
+	}
+
+}
---
+++ Final_Microservice/src/test/java/com/example/ocda/OcdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ocda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
